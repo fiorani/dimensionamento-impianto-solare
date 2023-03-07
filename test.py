f9017bef9d9d8c26c2f35def078496726bf94d7f
riccardo fiorani <riccardofiorani@hotmail.it>
2023-03-07 11:47:16 +0100

    add file

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ modules.load() # load all AMPL modules
 ampl = AMPL() # instantiate AMPL object
 ampl.read('test.mod') # Caricare il modello da file
 ampl.read_data('test.dat') # Caricare i dati da file
-ampl.option["solver"] = "highs" # Risolvere il modello
+ampl.option["solver"] = "cplex" # Risolvere il modello
 ampl.solve()
 print("Total_Profit is:", ampl.get_objective('Total_Profit').get().value())
 print("x is:", ampl.get_variable('X').get_values())
